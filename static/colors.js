f119d3f151ef8b99c75ee1f7b7a36ab36b071566
Bicrick <patrickbrown@utexas.edu>
2023-02-02 05:57:50 -0600

    Colorful background v5

--- static/colors.js
+++ static/colors.js
@@ -1,39 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     const body = document.querySelector("body");
 
-// define an array of colors
-const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    const background = document.querySelector("#background");
 
-let index = 0;
-
-// create a function to update the background color
-const updateColor = () => {
-  body.style.background = colors[index];
-  index = (index + 1) % colors.length;
-};
-
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    let startColor = "rgb(255, 255, 255)";
+    let endColor = "rgb(100, 149, 237)";
+    
+    background.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
+    
+    setInterval(() => {
+      startColor = endColor;
+      endColor = getRandomColor();
+      background.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
+    }, 5000);
+    
+    function getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
     }
-    return color;
-  }
-
-function getNegativeColor(color) {
-    const r = (255 - parseInt(color.substring(1, 3), 16)).toString(16).padStart(2, '0');
-    const g = (255 - parseInt(color.substring(3, 5), 16)).toString(16).padStart(2, '0');
-    const b = (255 - parseInt(color.substring(5, 7), 16)).toString(16).padStart(2, '0');
-    return `#${r}${g}${b}`;
-}
-
-// transition the color change over a period of time
-setInterval(function() {
-    const color = getRandomColor();
-    body.style.backgroundColor = color;
-    setTimeout(() => {
-        body.style.backgroundColor = getNegativeColor(color);
-      }, 1000);
-    }, 2000);
 });
