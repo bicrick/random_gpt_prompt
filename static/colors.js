3d41a1c62fd883fa0347af0c18d79bed97ee1e6b
Bicrick <patrickbrown@utexas.edu>
2023-02-02 06:11:46 -0600

    color changes

--- static/colors.js
+++ static/colors.js
@@ -2,22 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const body = document.querySelector("body");
 
 let startColor = 'white';
-let endColor = '#0077be';
+let endColor = 'gray';
 
 body.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
 
 setInterval(() => {
   startColor = endColor;
-  endColor = white;
+  endColor = getRandomGray();
   body.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
 }, 5000);
 
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function getRandomGray() {
+    let gray = Math.floor(Math.random() * 256).toString(16);
+    gray = gray.length === 1 ? "0" + gray : gray;
+    return "#" + gray + gray + gray;
   }
-  return color;
-}
 });
