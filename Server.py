8600bc144b536e78c7f3b4940a90afe1253d59e9
Bicrick <patrickbrown@utexas.edu>
2023-02-02 03:02:33 -0600

    Changed Server to a python file

---
+++ Server.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import random
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+df = pd.read_csv("prompts.csv")
+a_list = df['act'].values.tolist()
+b_list = df['prompt'].values.tolist()
+purpose = random.choice(a_list)
+prompt = random.choice(b_list)
+print(purpose + ": " + prompt)
+
+app = Flask(__name__)
+CORS(app)
+
+def RandomPrompt():
+    purpose = random.choice(a_list)
+    prompt = random.choice(b_list)
+
+    return [purpose,prompt]
+
+@app.route("/")
+def sendJSON():
+    response = RandomPrompt()
+    return jsonify({"message": response})
+
+if __name__ == '__main__':
+    app.run() 
