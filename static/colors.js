feecb6505d839f0969d76fbc8e6703c391367726
Bicrick <patrickbrown@utexas.edu>
2023-02-02 06:03:33 -0600

    Colorful background v5.1

--- static/colors.js
+++ static/colors.js
@@ -1,25 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     const body = document.querySelector("body");
 
-    const background = document.querySelector("#background");
+let startColor = 'white';
+let endColor = '#0077be';
 
-    let startColor = "rgb(255, 255, 255)";
-    let endColor = "rgb(100, 149, 237)";
-    
-    background.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
-    
-    setInterval(() => {
-      startColor = endColor;
-      endColor = getRandomColor();
-      background.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
-    }, 5000);
-    
-    function getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
+body.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
+
+setInterval(() => {
+  startColor = endColor;
+  endColor = getRandomColor();
+  body.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
+}, 5000);
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 });
