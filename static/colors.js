09f390bddf07de3f76c705720dba2e408755a9db
Bicrick <patrickbrown@utexas.edu>
2023-02-02 06:15:52 -0600

    Going simpler

--- static/colors.js
+++ static/colors.js
@@ -1,20 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const body = document.querySelector("body");
-
-let startColor = 'white';
-let endColor = 'gray';
-
-body.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
-
-setInterval(() => {
-  startColor = endColor;
-  endColor = getRandomGray();
-  body.style.background = `linear-gradient(to right, ${startColor}, ${endColor})`;
-}, 5000);
-
-function getRandomGray() {
-    let gray = Math.floor(Math.random() * 256).toString(16);
-    gray = gray.length === 1 ? "0" + gray : gray;
-    return "#" + gray + gray + gray;
-  }
+\
 });
