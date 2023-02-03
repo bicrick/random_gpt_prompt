5ef2a46344e794492c8eea6156c92e93826cfb3a
Bicrick <patrickbrown@utexas.edu>
2023-02-02 18:36:48 -0600

    Branch specifically for local development without heroku

--- Server.py
+++ Server.py
@@ -33,5 +33,5 @@ def home():
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port) 
+    app.run() 
 
--- static/script.js
+++ static/script.js
@@ -1,52 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var button = document.getElementById("myBtn");
-  
-    button.addEventListener("click", function() {
-      // Make an API call to your backend
+  var button = document.getElementById("myBtn");
 
-      fetch(`https://random-gpt-prompts.herokuapp.com/api`)
-        .then(response => response.json())
-        .then(data => {
-          // Update the text fields with the purpose and prompt
-            console.log(data.message[0])
-            console.log(data.message[1])
+  button.addEventListener("click", function() {
+    // Make an API call to your backend
+
+    fetch(`http://127.0.0.1:5000/api`)
+      .then(response => response.json())
+      .then(data => {
+        // Update the text fields with the purpose and prompt
+          console.log(data.message[0])
+          console.log(data.message[1])
 
 
-          document.getElementById("purpose").innerHTML = data.message[0];
-          document.getElementById("prompt").innerHTML = data.message[1];
-          document.getElementById("copyBtn").style.display = "inline-block";
+        document.getElementById("purpose").innerHTML = data.message[0];
+        document.getElementById("prompt").innerHTML = data.message[1];
+        document.getElementById("copyBtn").style.display = "inline-block";
 
-          document.getElementById("copyBtn").classList.remove("success");
-          document.getElementById("main").style.display = "block";
-          document.getElementById("main").style.display = "block";
-          //console.log("Test")
-        });
-    });
-
-    const copyBtn = document.getElementById("copyBtn");
-    const prompt = document.getElementById("prompt");
-
-    copyBtn.addEventListener("click", function() {
-
-        // Create a hidden textarea element
-        const textArea = document.createElement("textarea");
-        textArea.value = prompt.innerText;
-        document.body.appendChild(textArea);
-
-        // Select the text in the textarea
-        textArea.select();
-
-        // Copy the text to the clipboard
-        document.execCommand("copy");
-
-        // Remove the textarea element
-        document.body.removeChild(textArea);
-    });
-
-    document.querySelector("#copyBtn").addEventListener("click", function() {
-        // your code to copy text to clipboard
-        this.classList.add("success");
+        document.getElementById("copyBtn").classList.remove("success");
+        document.getElementById("main").style.display = "block";
+        document.getElementById("main").style.display = "block";
+        //console.log("Test")
       });
-    
   });
+
+  const copyBtn = document.getElementById("copyBtn");
+  const prompt = document.getElementById("prompt");
+
+  copyBtn.addEventListener("click", function() {
+
+      // Create a hidden textarea element
+      const textArea = document.createElement("textarea");
+      textArea.value = prompt.innerText;
+      document.body.appendChild(textArea);
+
+      // Select the text in the textarea
+      textArea.select();
+
+      // Copy the text to the clipboard
+      document.execCommand("copy");
+
+      // Remove the textarea element
+      document.body.removeChild(textArea);
+  });
+
+  document.querySelector("#copyBtn").addEventListener("click", function() {
+      // your code to copy text to clipboard
+      this.classList.add("success");
+    });
+  
+});
